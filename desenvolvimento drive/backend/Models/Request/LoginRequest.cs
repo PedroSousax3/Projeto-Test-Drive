396193d59b2731732e11b94d59730987465bc815
Pedro Sousa <pedro.sousax3@gmail.com>
2020-09-28 09:08:43 -0300

    Atualizando

--- desenvolvimento drive/backend/Models/Request/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace backend.Models.Request
-{
-    public class LoginRequest
-    {
-        public string usuario { get; set; }
-        public string senha { get; set; }
-    }
-}
--- desenvolvimento drive/backend/Models/Response/LoginResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace backend.Models.Response
-{
-    public class LoginResponse
-    {
-        public string usuario { get; set; }
-        public string cargo { get; set; }
-    }
-}
